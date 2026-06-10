e680d77b224211aa96be9dd83899dbe403129cd4
Danish <danishshamshee123@gmail.com>
2026-06-10 23:26:51 +0530

    add faculties pictures and their data in the JSON

---
+++ lib/faculty-data.ts
@@ -0,0 +1,232 @@
+
+export const facultyData  = [
+
+    // TEACHING STAFF
+    {
+        name: "Dr. Pankaj Kumar Samdaria",
+        designation: "Assistant Professor (Guest faculty)",
+        department: "Commerce ",
+        email: "samdariapankaj74@gmail.com",
+        phone: "",
+        type: "TEACHING",
+        image: ""
+    },
+    {
+        name: "Dr. Manoj Kumar",
+        designation: "Assistant Professor (Guest faculty)",
+        department: "History",
+        email: "",
+        phone: "7909006243",
+        type: "TEACHING",
+        image: ""
+    },
+    {
+        name: "Dr. Shashikant Kumar Priyadarshi",
+        designation: "Assistant Professor (Guest Faculty)",
+        department: "Political Science",
+        email: "",
+        phone: "",  
+        type: "TEACHING",
+        image: "ShashikantKumarPriyadarshi.jpg"
+    },
+    {
+        name: "DR. MANDEEP KUMAR",
+        designation: "ASSISTANT PROFESSOR (GUEST FACULTY)",
+        department: "PSYCHOLOGY",
+        email: "",
+        phone: "",
+        type: "TEACHING",
+        image: "MandeepKumar.jpg"
+    },
+    {
+        name: "Dr. Kunal Bharti",
+        designation: "Assistant Professor (Guest Faculty)",
+        department: "Department of Hindi",
+        email: "kunalbharti075@gmail.com",
+        phone: "9534914075",
+        type: "TEACHING",
+        image: ""
+    },
+    {
+        name: "Dr. Abhinav Das",
+        designation: "Assistant Professor (Guest Faculty)",
+        department: "Department of Psychology",
+        email: "abhinavdas@hotmail.com",
+        phone: "8130323599",
+        type: "TEACHING",
+        image: "AbhinavDas.jpg"
+    },
+    {
+        name: "	Dr. Ram Babu Singh",
+        designation: "Assistant Professor (Guest)",
+        department: "Department of Mathematics",
+        email: "ram9934ram1144@gmail.com",
+        phone: "9708920821",
+        type: "TEACHING",
+        image: "RamBabuSingh.jpg"
+    },
+    {
+        name: "Dr. Sunny Kumar",
+        designation: "Assistant professor (Guest)",
+        department: "Department of Psychology",
+        email: "sunnypandey1996@gmail.com",
+        phone: "9471498312",
+        type: "TEACHING",
+        image: "SunnyKumar.jpg"
+    },
+    {
+        name: "Dr. Suraj Kumar",
+        designation: "Assistant Professor (Guest Faculty)",
+        department: "Department of Zoology",
+        email: "639suraj@gmail.com",
+        phone: "7004285957",
+        type: "TEACHING",
+        image: "SurajKumar.jpg"
+    },
+    {
+        name: "Dr. Abhishek Kumar",
+        designation: "Assistant Professor (Guest)",
+        department: "Department of English",
+        email: "abhi18797shek@gmail.com",
+        phone: "8709105801",
+        type: "TEACHING",
+        image: "Abhishekkumar.jpg"
+    },
+    {
+        name: "Dr. Manoj Kumar Pandey",
+        designation: "Assistant Professor",
+        department: "Department of English",
+        email: "mkpandey139a@gmail.com",
+        phone: "9838319498",
+        type: "TEACHING",
+        image: "ManojKumarPandey.jpg"
+    },
+    {
+        name: "Mr. Hira Lal",
+        designation: "Assistant Professor",
+        department: "Department of Economics",
+        email: "gurujihira@gmail.com",
+        phone: "9911625646",
+        type: "TEACHING",
+        image: "HiraLal.jpg"
+    },
+    {
+        name: "Mr. Praful Kumar",
+        designation: "Assistant Professor",
+        department: "Department of Political Science",
+        email: "pankajpraful03@gmail.com",
+        phone: "8882539344",
+        type: "TEACHING",
+        image: "PrafulKumar.jpg"
+    },
+    {
+        name: "Dr. Tikeshwar Prasad Janghel",
+        designation: "Assistant Professor",
+        department: "Department of Hindi",
+        email: "tikku.janghel@gmail.com",
+        phone: "7354181058",
+        type: "TEACHING",
+        image: "TikeshwarPrasadJanghel.jpg"
+    },
+    {
+        name: "Dr. Nawal Kishore Pintu",
+        designation: "Assistant Professor",
+        department: "Department of Philosophy",
+        email: "pintuaryan7@gmail.com",
+        phone: "9631452576",
+        type: "TEACHING",
+        image: "NawalKishorePintu.jpg"
+    },
+    {
+        name: "Dr. Pallav Kumar",
+        designation: "Assistant Professor (Level 10)",
+        department: "Department of Psychology",
+        email: "psypallav@gmail.com",
+        phone: "9905043045",
+        type: "TEACHING",
+        image: "PallavKumar.jpg"
+    },
+    {
+        name: "Dr. Shabbir Ahmad",
+        designation: "Assistant Professor (Level 10)",
+        department: "Department of Urdu",
+        email: "shabbir9334907650@gmail.com",
+        phone: "9334907650",
+        type: "TEACHING",
+        image: "ShabbirAhmad.jpg"
+    },
+    {
+        name: "Dr. Manoj Kumar Yadav",
+        designation: "Assistant Professor (Level 10)",
+        department: "Department of Economics",
+        email: "manphd123@gmail.com",
+        phone: "9934658767",
+        type: "TEACHING",
+        image: "ManojKumarYadav.jpg"
+    },
+    {
+        name: "Dr. Ali Ahmad Ansari",
+        designation: "Assistant Professor (Level 10)",
+        department: "Department of Physics",
+        email: "aliahmadknet@gmail.com",
+        phone: "8340377407",
+        type: "TEACHING",
+        image: "AliAhmadAnsari.jpg"
+    },
+    {
+        name: "Dr. Arnab Maity",
+        designation: "Assistant Professor (Level 10)",
+        department: "Department of Chemistry",
+        email: "arnab.maity09@gmail.com",
+        phone: "7001397134",
+        type: "TEACHING",
+        image: "ArnabMaity.jpg"
+    },
+
+    // NON TEACHING STAFF
+    {
+        name: "Ravi Chandra Mandal",
+        designation: "Peon",
+        department: "",
+        email: "",
+        phone: "",
+        type: "NON-TEACHING",
+        image: ""
+    },
+    {
+        name: "Laxman Kumar Yadav",
+        designation: "Lab boy",
+        department: "",
+        email: "",
+        phone: "",
+        type: "NON-TEACHING",
+        image: ""
+    },
+    {
+        name: "Sri Shashibhusan Kumar",
+        designation: "L.D.C",
+        department: "",
+        email: "",
+        phone: "",
+        type: "NON-TEACHING",
+        image: ""
+    },
+    {
+        name: "Sri Akhilesh Kumar Mandal",
+        designation: "Peon",
+        department: "",
+        email: "",
+        phone: "",
+        type: "NON-TEACHING",
+        image: ""
+    },
+    {
+        name: "Sri Amarjeet Kumar",
+        designation: "L.D.C",
+        department: "",
+        email: "",
+        phone: "",
+        type: "NON-TEACHING",
+        image: ""
+    },
+]
